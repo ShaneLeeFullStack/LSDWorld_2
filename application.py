25699cac69f2c103c5a9e91ca1ba302fe5bc1e68
swcutler <swcutler@ucsc.edu>
2021-10-14 16:46:39 -0700

    templates/adding local_tripsitters.html

--- application.py
+++ application.py
@@ -3,10 +3,10 @@ from flask import Flask, url_for,request, render_template
 app = Flask(__name__)
 @app.route('/')
 def hello():
-    return "Hello World 1"
+    return "LSD WORLD is the networking app for psychonauts"
 @app.route('/index', methods=['GET','POST'])
 def index():
-    return "Hey This is an index page"
+    return render_template('local_tripsitters.html')
 if __name__ == "__main__":
     #app.run(host="ec2-54-215-40-76.us-west-1.compute.amazonaws.com", port=80)
  	app.run(host="0.0.0.0", port=80)
