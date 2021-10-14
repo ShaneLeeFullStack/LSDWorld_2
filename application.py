0ccf93fd14b7f6cf274eace53d10ca1a6d6748de
swcutler <swcutler@ucsc.edu>
2021-10-14 00:53:11 -0700

    import url_for, request

--- application.py
+++ application.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask, url_for,request
+
 app = Flask(__name__)
 @app.route('/')
 def hello():
     return "Hello World 1"
 @app.route('/index')
+
 def index():
     return "Hey This is an index page"
 if __name__ == "__main__":
