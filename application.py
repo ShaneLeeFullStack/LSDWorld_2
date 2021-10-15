d35e76019f4c445e82ce48ff63d1fcfce5fe7385
swcutler <swcutler@ucsc.edu>
2021-10-15 12:37:45 -0700

    %extends layout_auth.html%

--- application.py
+++ application.py
@@ -2,10 +2,11 @@ from flask import Flask, url_for,request, render_template
 app = Flask(__name__)
 @app.route('/')
 def hello():
-    return "LSD WORLD is the networking app for psychonauts"
+    return "LSD WORLD is the networking app for psychonauts" \
+           "Also, Julian Hodge is so damn sexy"
 @app.route('/index', methods=['GET','POST'])
 def index():
-    return "local tripsitters"
+    return render_template('local_tripsitters.html')
     #return render_template('local_tripsitters.html')
 if __name__ == "__main__":
  	app.run(host="0.0.0.0", port=80)
