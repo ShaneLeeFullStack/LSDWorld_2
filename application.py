87bf29008bab5b97bf31ee4d7323a63a1454b952
swcutler <swcutler@ucsc.edu>
2021-10-15 16:11:39 -0700

    tried to fix 404 errors for get js/index.js

--- application.py
+++ application.py
@@ -1,27 +1,27 @@
 from flask import Flask, url_for,request, render_template
 app = Flask(__name__)
-@app.route('/')
+@app.route('/', methods =['GET', 'POST'])
 def home_page():
     return render_template('layout_auth.html')
-@app.route('/local_tripsitters')
+@app.route('/local_tripsitters', methods =['GET', 'POST'])
 def local_tripsitters():
     return render_template('local_tripsitters.html')
-@app.route('/submit_trip_report_page')
+@app.route('/submit_trip_report_page', methods =['GET', 'POST'])
 def submit_trip_report():
     return render_template('submit_trip_report_form.html')
-#@app.route('/home_page')
+#@app.route('/home_page', methods =['GET', 'POST'])
 #def home_page():
 #    return render_template('index.html')
-@app.route('/journey_safe_page')
+@app.route('/journey_safe_page', methods =['GET', 'POST'])
 def journey_safe():
     return render_template('journey_safe_form.html')
-@app.route('/map_page')
+@app.route('/map_page', methods =['GET', 'POST'])
 def map_page():
     return render_template('map_form.html')
-@app.route('/create_profile_page')
+@app.route('/create_profile_page', methods =['GET', 'POST'])
 def create_profile_page():
     return render_template('create_profile_form.html')
-@app.route('/need_help')
+@app.route('/need_help', methods =['GET', 'POST'])
 def need_help():
     return render_template('need_help.html')
 #@app.route('/auth/logout')
