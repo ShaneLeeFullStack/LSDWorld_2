010f8cb6c3a0fe195bae4919ebb610c3a6492135
swcutler <swcutler@ucsc.edu>
2021-10-15 14:36:04 -0700

    added most url routes

--- application.py
+++ application.py
@@ -6,6 +6,27 @@ def home_page():
 @app.route('/local_tripsitters')
 def local_tripsitters():
     return render_template('local_tripsitters.html')
+@app.route('/submit_trip_report_page')
+def submit_trip_report():
+    return render_template('submit_trip_report_form.html')
+@app.route('/home_page')
+def home_page():
+    return render_template('index.html')
+@app.route('/journey_safe_page')
+def journey_safe():
+    return render_template('journey_safe_form.html.html')
+@app.route('/map_page')
+def map_page():
+    return render_template('map_form.html')
+@app.route('/create_profile_page')
+def create_profile_page():
+    return render_template('create_profile_form.html')
+@app.route('/need_help')
+def need_help():
+    return render_template('need_help.html')
+@app.route('/auth/logout')
+def auth_logout():
+    return render_template('auth.html')
 if __name__ == "__main__":
  	app.run(host="0.0.0.0", port=80)
 
