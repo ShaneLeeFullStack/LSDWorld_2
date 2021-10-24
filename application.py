863f21d715abe04fff850f5d54b044b50fce6f87
swcutler <swcutler@ucsc.edu>
2021-10-24 00:25:32 -0700

    insert hard-coded values and insert request.form[ ] values

--- application.py
+++ application.py
@@ -1,9 +1,18 @@
-from sqlalchemy import ForeignKey, create_engine, inspect, insert
+from sqlalchemy import ForeignKey, create_engine, Integer, String, insert, Column, MetaData, Table
+from sqlalchemy.orm import Session
 import datetime
 from flask import Flask, url_for, request, render_template, redirect
 from flask_sqlalchemy import SQLAlchemy
 import pyodbc
 import urllib.parse
+from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
+
+def get_time():
+    return datetime.datetime.utcnow()
+
+
+
 
 # Configure Database URI:
 params = urllib.parse.quote_plus("Driver={ODBC Driver 17 for SQL Server};"
@@ -23,9 +32,30 @@ conn_str = 'mssql+pyodbc:///?odbc_connect={}'.format(params)
 engine_azure = create_engine(conn_str, echo=True)
 db = SQLAlchemy(app)
 
+meta = MetaData()
 
-def get_time():
-    return datetime.datetime.utcnow()
+
+sub_table = Table('sub_table', meta,
+                   Column('substance_id', Integer, primary_key=True),
+                   Column('substance_name', String))
+meta.create_all(engine_azure)
+
+#class trip_reports_class(Base):
+#    __tablename__ = 'trip_reports_class'
+    #trip_report_id = db.Column(db.Integer, primary_key=True,
+    #                           autoincrement=True
+    #                           )
+    #date = db.Column(db.Text, default=get_time())
+    #user_id = db.Column(db.Integer,
+    #                    # db.ForeignKey('user_profile.user_id'),
+    #                    nullable=False)
+    #title = db.Column(db.Text)
+    #substance_id = db.Column(db.Integer, db.ForeignKey('substance_table.substance_id'),
+    #                         nullable=False)
+    #report_content = db.Column(db.Text)
+    #is_showing = db.Column(db.Integer, db.ForeignKey('trip_reports.trip_report_id'))
+
+
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -45,11 +75,20 @@ def submit_trip_report_page():
 
 @app.route('/submit_trip_report', methods=['GET', 'POST'])
 def submit_trip_report():
+    #result = Session.query(trip_reports_class).filter(trip_reports_class.user_id == 12)
+    #with engine_azure.connect() as az_eng_conn:
     new_substance_name = request.form['substance_name']
-    # substance_id = dbo.SUBSTANCE_TABLE.query.filter_by(
+    #substance_id = sub_table.query.filter_by(
     #    substance_name=request.form['substance_name']
     # ).first().substance_id
-    # print(substance_id)
+    #print(substance_id)
+    #print(result)
+    insertion_statement = insert(sub_table).values(substance_id = request.form['substance_name'],
+                                                   substance_name = 'dmt')
+    engine_azure.connect().execute(insertion_statement)
+    result = Session.query(Base.metadata.tables[sub_table]).all()
+    print(result)
+    #engine_azure.connect().commit()
     title = request.form['title']
     report_content = request.form['report_content']
     engine_azure.execute("SET IDENTITY_INSERT dbo.TRIP_REPORTS ON")
@@ -60,7 +99,7 @@ def submit_trip_report():
                          "substance_id,"
                          "substance_name,"
                          "report_content)"
-                         "VALUES(1,12, 'second title' , 4,  'marijuana','first report content')")
+                         "VALUES(100,12, 'second title' , 4,  'marijuana','first report content')")
     db.session.commit()
     return redirect('submit_trip_report_page')
 
