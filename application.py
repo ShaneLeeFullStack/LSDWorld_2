bfdd734b84fbbba698a5978e54082261b072768d
Shane Lee Smith <smith@ucsc.edu>
2022-06-28 17:07:38 -0700

    better functionality

--- application.py
+++ application.py
@@ -111,6 +111,14 @@ def submit_trip_report():
 
 
 # defining home page
+
+@app.route('/', methods=['GET', 'POST'])
+def home_page():
+    sub_id_query = "SELECT * FROM TRIP_REPORTS";
+    fetched_trip_reports = engine_azure.connect().execute(sub_id_query)
+    return render_template('home_page.html', fetched_trip_reports=fetched_trip_reports)
+
+
 @app.route('/home_page', methods=['GET', 'POST'])
 def home_page():
     sub_id_query = "SELECT * FROM TRIP_REPORTS";
