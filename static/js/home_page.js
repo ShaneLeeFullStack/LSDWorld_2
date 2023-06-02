cf60dcc104b9aba96a57da4b4831d3ecb941fc5a
Shane Lee <ghostbatlee@outlook.com>
2023-06-02 10:35:19 -0700

    eliminating errors

--- static/js/home_page.js
+++ static/js/home_page.js
@@ -61,8 +61,8 @@ let init = (app) => {
 
   app.fetch_trip_reports = () => {
       axios
-          // .get('http://192.168.1.73/fetch_trip_reports')
-          .get (fetch_trip_reports)
+          .get('http://192.168.1.73/fetch_trip_reports')
+          //.get (fetch_trip_reports)
           .then((result) => {
               app.vue.trip_reports = result.data.trip_reports
           })
